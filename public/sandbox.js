8fb100a0537170b6cd88a4697747dbb3e5dcebd2
damarisfreg <fregosodmar24@gmail.com>
2023-11-29 23:56:47 -0800

    Learning Function Signatures

--- public/sandbox.js
+++ public/sandbox.js
@@ -12,9 +12,113 @@
 // console.log(circ(7.5));
 var names = ['luigi', 'mario', 'peach'];
 names.push('toad');
-var logDetails = function (uid, item) {
-    console.log("".concat(item, " has a uid of ").concat(uid));
+//Arrays
+//cannot do this because the names array was initialized to strings
+// names.push(3);
+//names[0] = 3;
+// let numbers = [10, 20, 30, 40];
+// numbers.push(25);
+// numbers.push('shaun');
+// numbers[1] = 'shaun'
+// let mixed = ['ken', 4, 'jenny', 8, 9];
+// mixed.push('ryu');
+// mixed.push(10);
+// mixed[0] = 3;
+// //Objects 
+// let ninja = {
+//   name: 'mario',
+//   belt: 'black',
+//   age: 30
+// };
+// ninja.age = 40;
+// ninja.name = 'ryu'
+// // ninja.age = '30'
+// // ninja.skills = ['fighting', 'sneaking']
+// ninja = {
+//   name: 'luigi',
+//   belt: 'orange',
+//   age: 40,
+//   skills: []
+// }
+//EXPLICIT TYPES
+// //explicit types
+// let character: string;
+// let age: number;
+// let isLoggedIn: boolean;
+// age = 22;
+// isLoggedIn = true;
+// //arrays
+// let ninjas: string[] = [];
+// ninjas.push('shaun');
+// //union types
+// let mixed: (string|number| boolean)[] =[];
+// mixed.push('hello');
+// mixed.push(20);
+// mixed.push(false);
+// let uid: string|number;
+// uid = '123';
+// uid = 123;
+// //objects
+// let ninjaOne: object;
+// ninjaOne ={ name: 'yoshi', age = 30}
+// let ninjaTwo: {
+//   name: string,
+//   age: number,
+//   beltColor: string
+// }
+// ninjaTwo = {name: 'luigi', age: 20, beltColor: 'black'}
+//DYNAMIC TYPES (try not to use so much defeats the purpose of typescript)
+// let age: any = 25;
+// age = true;
+// let mixed: any[] =[];
+// mixed.push('hello');
+// mixed.push(22);
+// mixed.push(false);
+// let ninja: {name: any, age: any}
+// ninja = {name: 'yoshi', age: 25};
+// ninja = {name: 25, age: 'yoshi'};
+// console.log('test');
+// let greet: Function;
+// greet = () => {
+//   console.log('hello, again');
+// }
+// //tip: always do required parameters first, then the optional at the end
+// const add = (a: number, b: number, c: number | string = 10): void => {
+//   console.log(a + b);
+//   console.log(c);
+// }
+// add(5, 10, '20');
+// const minus = (a: number, b: number) => {
+//   return a +b;
+// }
+// let result = minus(10, 7);
+// type StringOrNum = string | number;
+// type objWithName = {name: string, uid: StringOrNum}
+// const logDetails = (uid: StringOrNum, item: string) => {
+//   console.log(`${item} has a uid of ${uid}`);
+// }
+// const greet = (user: objWithName) => {
+//   console.log(`${user.name} says hello`);
+// }
+//Function Signatures
+// let greet: Function;
+//ex 1
+var greet;
+greet = function (name, greeting) {
+    console.log("".concat(name, " says ").concat(greeting));
 };
-var greet = function (user) {
-    console.log("".concat(user.name, " says hello"));
+//ex 2 
+var calc;
+calc = function (numOne, numTwo, action) {
+    if (action === 'add') {
+        return numOne + numTwo;
+    }
+    else {
+        return numOne - numTwo;
+    }
+};
+//ex 3
+var logDetails;
+logDetails = function (ninja) {
+    console.log("".concat(ninja.name, " is ").concat(ninja.age, " years old"));
 };
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -127,14 +127,41 @@ names.push('toad');
 // }
 
 // let result = minus(10, 7);
-type StringOrNum = string | number;
-type objWithName = {name: string, uid: StringOrNum}
+// type StringOrNum = string | number;
+// type objWithName = {name: string, uid: StringOrNum}
 
-const logDetails = (uid: StringOrNum, item: string) => {
-  console.log(`${item} has a uid of ${uid}`);
+// const logDetails = (uid: StringOrNum, item: string) => {
+//   console.log(`${item} has a uid of ${uid}`);
+// }
+
+// const greet = (user: objWithName) => {
+//   console.log(`${user.name} says hello`);
+// }
+
+//Function Signatures
+// let greet: Function;
+
+//ex 1
+let greet: (a: string, b: string) => void;
+greet = (name: string, greeting: string) => {
+  console.log(`${name} says ${greeting}`);
 }
 
-const greet = (user: objWithName) => {
-  console.log(`${user.name} says hello`);
+//ex 2 
+let calc: (a: number, b: number, c: string) => number;
+
+calc = (numOne: number, numTwo: number, action: string) => {
+  if (action === 'add'){
+    return numOne + numTwo;
+  } else {
+    return numOne - numTwo;
+  }
 }
 
+//ex 3
+let logDetails: (obj: {name: string, age: number}) => void;
+
+type person = {name: string, age: number}
+logDetails = (ninja: person) => {
+  console.log(`${ninja.name} is ${ninja. age} years old`)
+}
