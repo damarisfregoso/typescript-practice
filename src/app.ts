1160b9695cb90591b9a6fb01a070f7a29b64e272
damarisfreg <fregosodmar24@gmail.com>
2023-12-04 23:17:09 -0800

    Working with the dom & type casting

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,26 @@
-// const anchor = document.querySelector('a')!;
+const anchor = document.querySelector('a')!;
+if(anchor) {
+  console.log(anchor.href);
+}
+console.log(anchor.href);
 
-// // if(anchor) {
-// //   console.log(anchor.href)
-// // }
-
-// console.log(anchor.href)
-
-// const form = document.querySelector('form');
-
+//const form = document.querySelector('form')!;
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 console.log(form.children);
 
-//inputs
-const type = document.querySelector('#type') as HTMLSelectElement;
-const toform = document.querySelector('#toform') as HTMLSelectElement;
-const details = document.querySelector('#details') as HTMLSelectElement;
-const amount = document.querySelector('#amount') as HTMLSelectElement;
+// inputs
+const type = document.querySelector('#type') as HTMLInputElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+form.addEventListener('submit', (e: Event) => {
+  e.preventDefault();
+
+  console.log(
+    type.value, 
+    tofrom.value, 
+    details.value, 
+    amount.valueAsNumber
+  );
+});
