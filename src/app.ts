b2b8f5c206d2b6dffd24cc10a3675b6e721c5319
damarisfreg <fregosodmar24@gmail.com>
2023-12-05 23:59:14 -0800

    Creating Modules

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,4 @@
-class Invoice {
-  client: string;
-  details: string;
-  amount: number;
-
-  constructor(c: string, d: string, a: number){
-    this.client = c;
-    this.details = d;
-    this.amount = a;
-  }
-  
-  format() {
-    return `${this.client} owes ${this.amount} for ${this.details}`;
-  }
-}
+import { Invoice } from './classes/Invoice.js'
 
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
@@ -20,12 +6,6 @@ const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 let invoices: Invoice[] = [];
 invoices.push(invOne);
 invoices.push(invTwo);
-
-invOne.client = 'yoshi';
-invTwo.amount = 400;
-
-console.log(invOne, invTwo);
-console.log(invoices);
 
 //const form = document.querySelector('form')!;
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
