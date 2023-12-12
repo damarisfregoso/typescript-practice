ef52cb174fa7af8e9a678bd323846e220cd1b8bd
damarisfreg <fregosodmar24@gmail.com>
2023-12-11 23:50:25 -0800

    Learning tuples

--- src/app.ts
+++ src/app.ts
@@ -33,11 +33,14 @@ const amount = document.querySelector('#amount') as HTMLInputElement;
 form.addEventListener('submit', (e: Event) => {
   e.preventDefault();
 
+  let values: [string, string, number]
+  values = [tofrom.value, details.value, amount.valueAsNumber]
+
   let doc: HasFormatter;
   if (type.value === 'invoice'){
-    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber)
+    doc = new Invoice(...values)
   } else {
-    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)
+    doc = new Payment(...values)
   }
 
   console.log(doc);
@@ -74,3 +77,4 @@ const docFour: Resource<string[]> = {
 }
 
 console.log(docThree, docFour);
+
