c3ace114753c71d1c6b466d5311cdb94b746e238
damarisfreg <fregosodmar24@gmail.com>
2023-12-12 23:54:42 -0800

    Invoices and payments displaying on screen

--- public/app.js
+++ public/app.js
@@ -1,59 +1,36 @@
 import { Invoice } from './classes/Invoice.js';
 import { Payment } from './classes/Payment.js';
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-// docOne = new Invoice('yoshi', 'web work', 250);
-// docTwo = new Payment('mario', 'plumbing work', 200);
-// let docs: HasFormatter[] = [];
-// docs.push(docOne);
-// docs.push(docTwo);
-//console.log(docs);
-// const invOne = new Invoice('mario', 'work on the mario website', 250);
-// const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
-// let invoices: Invoice[] = [];
-// invoices.push(invOne);
-// invoices.push(invTwo);
+import { ListTemplate } from './classes/ListTemplate.js';
 const form = document.querySelector('.new-item-form');
-console.log(form.children);
 // inputs
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+// list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    let values;
+    values = [tofrom.value, details.value, amount.valueAsNumber];
     let doc;
     if (type.value === 'invoice') {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     }
     else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
-    console.log(doc);
+    list.render(doc, type.value, 'end');
 });
-const addUID = (obj) => {
-    let uid = Math.floor(Math.random() * 100);
-    return Object.assign(Object.assign({}, obj), { uid });
-};
-let docOne = addUID({ name: 'yoshi', age: 40 });
-// let docTwo = addUID('hello')
-console.log(docOne.age);
-var ResourceType;
-(function (ResourceType) {
-    ResourceType[ResourceType["BOOK"] = 0] = "BOOK";
-    ResourceType[ResourceType["AUTHOR"] = 1] = "AUTHOR";
-    ResourceType[ResourceType["FILM"] = 2] = "FILM";
-    ResourceType[ResourceType["DIRECTOR"] = 3] = "DIRECTOR";
-    ResourceType[ResourceType["PERSON"] = 4] = "PERSON";
-})(ResourceType || (ResourceType = {}));
-const docThree = {
-    uid: 1,
-    resourceType: ResourceType.BOOK,
-    data: 'shaun'
-};
-const docFour = {
-    uid: 2,
-    resourceType: ResourceType.PERSON,
-    data: ['bread', 'milk', 'toilet roll']
-};
-console.log(docThree, docFour);
+// TUPLES
+let arr = ['ryu', 25, true];
+arr[0] = false;
+arr[1] = 'yoshi';
+arr = [30, false, 'yoshi'];
+let tup = ['ryu', 25, true];
+// tup[0] = false;
+tup[0] = 'ken';
+let student;
+//student = [23564, 'chun-li'];
+student = ['chun-li', 23564];
--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,9 @@
-import { Invoice } from './classes/Invoice.js'
-import { Payment } from './classes/Payment.js'
-import { HasFormatter } from './interfaces/HasFormatter.js'
-
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-
-// docOne = new Invoice('yoshi', 'web work', 250);
-// docTwo = new Payment('mario', 'plumbing work', 200);
-
-// let docs: HasFormatter[] = [];
-// docs.push(docOne);
-// docs.push(docTwo);
-
-//console.log(docs);
-
-// const invOne = new Invoice('mario', 'work on the mario website', 250);
-// const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
-
-// let invoices: Invoice[] = [];
-// invoices.push(invOne);
-// invoices.push(invTwo);
+import { Invoice } from './classes/Invoice.js';
+import { Payment } from './classes/Payment.js';
+import { ListTemplate } from './classes/ListTemplate.js';
+import { HasFormatter } from './interfaces/HasFormatter.js';
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-console.log(form.children);
 
 // inputs
 const type = document.querySelector('#type') as HTMLInputElement;
@@ -30,51 +11,36 @@ const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
 
+// list template instance
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
+
 form.addEventListener('submit', (e: Event) => {
   e.preventDefault();
 
-  let values: [string, string, number]
-  values = [tofrom.value, details.value, amount.valueAsNumber]
+  let values: [string, string, number];
+  values = [tofrom.value, details.value, amount.valueAsNumber];
 
   let doc: HasFormatter;
-  if (type.value === 'invoice'){
-    doc = new Invoice(...values)
+  if (type.value === 'invoice') {
+    doc = new Invoice(...values);
   } else {
-    doc = new Payment(...values)
+    doc = new Payment(...values);
   }
-
-  console.log(doc);
+  
+  list.render(doc, type.value, 'end');
 });
 
-const addUID = <T extends {name: string}>(obj: T) => {
-  let uid = Math.floor(Math.random() * 100);
-  return {...obj, uid};
-}
+// TUPLES
+let arr = ['ryu', 25, true];
+arr[0] = false;
+arr[1] = 'yoshi';
+arr = [30, false, 'yoshi'];
 
-let docOne = addUID ({name: 'yoshi', age: 40});
-// let docTwo = addUID('hello')
+let tup: [string, number, boolean] = ['ryu', 25, true];
+// tup[0] = false;
+tup[0] = 'ken';
 
-console.log(docOne.age);
-
-enum ResourceType { BOOK, AUTHOR, FILM, DIRECTOR, PERSON }
-
-interface Resource<T> {
-  uid: number;
-  resourceType: ResourceType;
-  data: T;
-}
-
-const docThree: Resource<string> = {
-  uid: 1,
-  resourceType: ResourceType.BOOK,
-  data: 'shaun'
-}
-
-const docFour: Resource<string[]> = {
-  uid: 2,
-  resourceType: ResourceType.PERSON,
-  data: ['bread', 'milk', 'toilet roll']
-}
-
-console.log(docThree, docFour);
-
+let student: [string, number];
+//student = [23564, 'chun-li'];
+student = ['chun-li', 23564];
